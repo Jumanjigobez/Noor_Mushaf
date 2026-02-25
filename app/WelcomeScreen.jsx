548731b48b9870d0b24cdc6e08e4effcb7fed9e2
Jumanjigobez <jumagobe3@gmail.com>
2026-02-25 10:21:06 +0300

    Fixed router push link

--- app/WelcomeScreen.jsx
+++ app/WelcomeScreen.jsx
@@ -1,13 +1,15 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 
+import { useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const WelcomeScreen = ({ navigation }) => {
+const WelcomeScreen = () => {
+  const router = useRouter();
   return (
     <SafeAreaView style={styles.main}>
       <View>
         <Text>Bismillah!</Text>
-        <Button title="Go Read" onPress={() => navigation.navigate("Reader")} />
+        <Button title="Go Read" onPress={() => router.push("/ReaderScreen")} />
       </View>
     </SafeAreaView>
   );
